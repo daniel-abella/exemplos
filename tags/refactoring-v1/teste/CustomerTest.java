a97e3a85e9425c1b0404eb42f9229c96e714ad91
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2007-09-11 18:09:08 +0000

    divide statement(). Cria amountFor()

---
+++ tags/refactoring-v1/teste/CustomerTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CustomerTest {
+	private Movie m1, m2, m3;
+	private Rental r1, r2;
+	private Customer c1, c2;
+	
+	private final String HEADER = "Rental Record for %s\n";
+	private final String MOVIE = "\t%s\t%s\n";
+	private final String FOOTER = "Amount owed is %s\nYou earned %s frequent renter points";
+	
+	@Before
+	public void setUp() {
+		m1 = new Movie("Homem Nordestino", Movie.REGULAR);
+		m2 = new Movie("Anaconda", Movie.NEW_RELEASE);
+		m3 = new Movie("Os três porquinhos", Movie.CHILDRENS);
+		
+		r1 = new Rental(m1, 1);
+		
+		c1 = new Customer("Fulano");
+		c2 = new Customer("Ciclano");
+	}
+	
+	@Test
+	public void testSentencaTrivial() {		
+		c1.addRental(r1);		
+		String saida = HEADER + MOVIE + FOOTER;
+		saida = String.format(saida, "Fulano", "Homem Nordestino", "2.0", "2.0", "1");
+		assertTrue(saida.equals(c1.statement()));
+	}
+}

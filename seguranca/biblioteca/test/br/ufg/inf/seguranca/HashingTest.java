b56b12e6455d31f6385a6a2453a9c61bf02b7f9e
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2008-09-05 11:55:09 +0000

    TEste

--- seguranca/biblioteca/test/br/ufg/inf/seguranca/HashingTest.java
+++ seguranca/biblioteca/test/br/ufg/inf/seguranca/HashingTest.java
@@ -1,41 +1,26 @@
 package br.ufg.inf.seguranca;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author usuário
+ * @author Fábio Nogueira de Lucena
+ * @version 0.1
  */
 public class HashingTest {
-
-    public HashingTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     @Test
     public void testMd5() throws Exception {
         Hashing hash = new HashingImpl();
         byte[] hashValue = hash.md5("res/teste.txt");
         assertEquals("1ca308df6cdb0a8bf40d59be2a17eac1", hash.toHex(hashValue));
+    }
+    
+    @Test
+    public void testSha1() throws Exception {
+        Hashing hash = new HashingImpl();
+        byte[] hashValue = hash.sha1("res/teste.txt");
+        assertEquals("9dc628289966d144c1a5fa20dd60b1ca1b9de6ed", hash.toHex(hashValue));
     }
 }

2ae2b3aa10994cbe25e7b0110c3ab4cbb913f548
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2009-01-27 21:01:46 +0000

    Testes OK. Novo formato binario ainda nao disponivel. Ultima revisao antes da alteracao.

--- trunk/ejb3/jpaTres/src/tres/Endereco.java
+++ trunk/ejb3/jpaTres/src/tres/Endereco.java
@@ -4,19 +4,22 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class Endereco {
+
     private String rua;
     private int numero;
-    
+
     // Construtor default é obrigatório
-    public Endereco() {}
-    
-    public Endereco(String rua, int numero) {
-	super();
-	this.rua = rua;
-	this.numero = numero;
+    public Endereco() {
     }
 
+    public Endereco(String rua, int numero) {
+        super();
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    @Override
     public String toString() {
-	return rua + " " + numero;
+        return rua + " " + numero;
     }
 }

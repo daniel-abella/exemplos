e0f37fd46285d8cf32107ee25d3d0bdbfe38c8e5
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2009-01-27 20:24:20 +0000

    Formato acrescentado (ainda não confirmado se acrescimo está OK)

--- trunk/ejb3/jpaDois/src/dois/Pessoa.java
+++ trunk/ejb3/jpaDois/src/dois/Pessoa.java
@@ -1,11 +1,12 @@
 package dois;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Pessoa {
+public class Pessoa implements Serializable {
 	@Id @GeneratedValue
 	private Long id;
 	
@@ -20,6 +21,7 @@ public class Pessoa {
 		this.nome = nome;
 	}
 	
+    @Override
 	public String toString() {
 		return nome;
 	}

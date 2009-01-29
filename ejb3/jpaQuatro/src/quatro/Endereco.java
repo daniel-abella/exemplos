d5a0ca542278071c0db55c85780ff7ab9a57be44
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2009-01-29 17:02:26 +0000

    Formato acrescentado (ainda não confirmado se acrescimo está OK)

--- ejb3/jpaQuatro/src/quatro/Endereco.java
+++ ejb3/jpaQuatro/src/quatro/Endereco.java
@@ -1,11 +1,12 @@
 package quatro;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Endereco {
+public class Endereco implements Serializable {
 	@Id
 	@GeneratedValue
 	private Long id;

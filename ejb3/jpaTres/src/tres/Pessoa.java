f74958889c66d5ca4e5a710065db53e211975864
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2009-03-08 14:07:04 +0000

    Testes OK. Novo formato binario ainda nao disponivel. Ultima revisao antes da alteracao.

--- ejb3/jpaTres/src/tres/Pessoa.java
+++ ejb3/jpaTres/src/tres/Pessoa.java
@@ -1,12 +1,13 @@
 package tres;
 
+import java.io.Serializable;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Pessoa {
+public class Pessoa implements Serializable {
 	@Id @GeneratedValue
 	private Long id;
 	

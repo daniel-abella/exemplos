b52a4055fae44d877944906f853fd8c13e0838df
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2009-03-08 14:55:47 +0000

    Testes OK. Novo formato binario ainda nao disponivel. Ultima revisao antes da alteracao.

--- ejb3/jpaCinco/src/cinco/Endereco.java
+++ ejb3/jpaCinco/src/cinco/Endereco.java
@@ -1,11 +1,12 @@
 package cinco;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Endereco {
+public class Endereco implements Serializable {
 	@Id
 	@GeneratedValue
 	private Long id;

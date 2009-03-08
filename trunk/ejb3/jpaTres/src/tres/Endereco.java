9fa0a266c5d759e18a071f1d4519c868d4a487c4
projeto.kyrios <projeto.kyrios@e2954138-3125-0410-b7ab-251e373a8e33>
2009-03-08 14:07:04 +0000

    Testes OK. Novo formato binario ainda nao disponivel. Ultima revisao antes da alteracao.

--- trunk/ejb3/jpaTres/src/tres/Endereco.java
+++ trunk/ejb3/jpaTres/src/tres/Endereco.java
@@ -1,9 +1,10 @@
 package tres;
 
+import java.io.Serializable;
 import javax.persistence.Embeddable;
 
 @Embeddable
-public class Endereco {
+public class Endereco implements Serializable {
 
     private String rua;
     private int numero;
